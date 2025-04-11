793d07054ec9bf60d8267da11f5780e9272f58c0
akash <ak45hhere@gmail.com>
2025-04-11 19:58:25 +0530

    remove console log for Google client ID in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,10 +7,6 @@ createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider
     clientId={`${import.meta.env.VITE_GOOGLE_CLIENT_ID}`}
   >
- {
-  
-  console.log(import.meta.env.VITE_GOOGLE_CLIENT_ID)
-} 
     <App />
   </GoogleOAuthProvider>
 )
